100431df9ba9f94b525767bf2c373acf863ce8f6
Parag <paragdutta@iisc.ac.in>
2026-06-09 17:57:26 +0000

    created separate train and infer scripts; modified tokenizer, dataloader, and model for using huggingface pipeline for text-generation

--- utils.py
+++ utils.py
@@ -1,15 +1,5 @@
 import torch
-import random
 from typing import Tuple
-
-
-def set_seed(seed: int):
-    random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-        # torch.backends.cudnn.deterministic = True
-        # torch.backends.cudnn.benchmark = False
 
 
 def train_test_split(
