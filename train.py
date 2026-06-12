0e3fff5d02c40874d020920c38ed2491ddca1353
Parag <paragdutta@iisc.ac.in>
2026-06-12 17:46:27 +0000

    fixed a small bug in lr scheduler

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
-import os
-import argparse
+import os, math, argparse
 import torch, torch.optim as optim
-from torch.optim.lr_scheduler import ConstantLR, LinearLR, CosineAnnealingLR, SequentialLR
+from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, SequentialLR
 from transformers import set_seed, TrainingArguments, Trainer
 
 from model import Model, ModelConfig
@@ -22,6 +21,9 @@ def main(args: argparse.Namespace):
         num_attention_heads=args.n_heads,
         dropout=args.dropout
     ))
+    if os.path.exists("models/checkpoint-35000"):
+        print("Loading model from checkpoint...")
+        model = Model.from_pretrained("models/checkpoint-35000")
     model.config.eos_token_id = tokenizer.eos_token_id
     model.config.pad_token_id = tokenizer.pad_token_id
     model.generation_config.eos_token_id = tokenizer.eos_token_id
@@ -34,8 +36,9 @@ def main(args: argparse.Namespace):
     cosine_scheduler = CosineAnnealingLR(
         optimizer, T_max=args.last_decay_iter - args.warmup_iters,
         eta_min=args.learning_rate / 10)
-    constant_scheduler = ConstantLR(
-        optimizer, factor=1.0, total_iters=args.n_iters - args.last_decay_iter)
+    constant_scheduler = LinearLR(
+        optimizer, start_factor=0.1, end_factor=0.1,
+        total_iters=args.n_iters - args.last_decay_iter)
     scheduler = SequentialLR(
         optimizer,
         schedulers=[warmup_scheduler, cosine_scheduler, constant_scheduler],
@@ -48,10 +51,10 @@ def main(args: argparse.Namespace):
         max_steps=args.n_iters,
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
-        # eval_on_start=True,
-        # eval_strategy="steps",
-        # eval_steps=args.eval_interval,
-        gradient_accumulation_steps=2,
+        eval_on_start=True,
+        eval_strategy="steps",
+        eval_steps=args.eval_interval,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
         save_total_limit=2,
         save_steps=args.save_interval,
         learning_rate=args.learning_rate,
@@ -83,22 +86,22 @@ if __name__ == "__main__":
     parser.add_argument("--save-dir", type=str, default="models")
     parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--vocab-size", type=int, default=49216)
-    parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--block-size", type=int, default=1024)
     parser.add_argument("--n-embed", type=int, default=768)
     parser.add_argument("--n-heads", type=int, default=12)
     parser.add_argument("--n-layers", type=int, default=12)
     parser.add_argument("--dropout", type=float, default=0.2)
-    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--learning-rate", type=float, default=3e-4)
     parser.add_argument("--weight-decay", type=float, default=0.1)
     parser.add_argument("--max-grad-norm", type=float, default=1.0)
-    parser.add_argument("--n-iters", type=int, default=37650)
+    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--effective-tokens-target", type=int, default=2**19)
+    parser.add_argument("--n-iters", type=int, default=38145)
     parser.add_argument("--warmup-iters", type=int, default=500)
-    parser.add_argument("--last-decay-iter", type=int, default=34000)
-    parser.add_argument("--log-interval", type=int, default=100)
-    # parser.add_argument("--eval-interval", type=int, default=100)
-    # parser.add_argument("--eval-iters", type=int, default=5)
-    parser.add_argument("--save-interval", type=int, default=1000)
+    parser.add_argument("--last-decay-iter", type=int, default=35000)
+    parser.add_argument("--log-interval", type=int, default=10)
+    parser.add_argument("--eval-interval", type=int, default=500)
+    parser.add_argument("--save-interval", type=int, default=5000)
     parser.add_argument("--use-bf16", action="store_true")
     parser.add_argument("--wandb-run-name", type=str, default=None)
     parser.add_argument("--wandb-project", type=str, default="improved-transformer")
@@ -108,4 +111,11 @@ if __name__ == "__main__":
     set_seed(args.seed)
     os.environ["WANDB_PROJECT"] = args.wandb_project
     os.environ["WANDB_ENTITY"] = args.wandb_entity
+    effective_tokens_target = args.effective_tokens_target
+    effective_batch_size = args.batch_size
+    if torch.cuda.is_available():
+        effective_batch_size *= torch.cuda.device_count()
+    effective_tokens_size = effective_batch_size * args.block_size
+    args.gradient_accumulation_steps =\
+        max(1, math.ceil(effective_tokens_target / effective_tokens_size))
     main(args)
