8cf7274e188234d16432cb0dda12cbee5e856aa1
Parag <paragdutta@iisc.ac.in>
2026-06-21 11:31:34 +0000

    added hellaswag evaluation

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,12 @@
 # get all the functions and classes from the utils submodules and expose them
-from .data_utils import get_tokenizer, TokenDataset
+from .data_utils import get_tokenizer, TokenDataset, HellaswagDataset, hellaswag_collate_fn
 from .train_utils import LLMTrainer
 from .misc_utils import human_readable_numbers
 __all__ = [
     "get_tokenizer",
     "TokenDataset",
+    "HellaswagDataset",
+    "hellaswag_collate_fn",
     "LLMTrainer",
     "human_readable_numbers"
 ]
